9ba4423b3c85f7b931328457068ef828c7ca2359
luanMarvin <luanmarvin@gmail.com>
2023-05-25 22:54:45 -0300

    Finalização configuração do prisma

--- server/src/controllers/infoController.js
+++ server/src/controllers/infoController.js
@@ -1,7 +1,15 @@
-async function getInfo(ctx){
-    ctx.body = "Receber informações do Banco de dados"
-};
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function getInfo(ctx) {
+    try {
+        const result = await prisma.$queryRaw`SELECT 1`;
+        ctx.body = `Connection to the database is successful. Result: ${result[0]['?column?']}`;
+    } catch (error) {
+        ctx.body = `Error connecting to the database: ${error.message}`;
+    }
+}
 
 module.exports = {
     getInfo
-}
+};
